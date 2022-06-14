ea4b9b0bc5d758be248ff03b900fcac7de3de874
KlinkerJ <jonasklinker@gmail.com>
2022-06-14 18:36:38 +0200

    Got a fast reacting robot working!

--- move_robot.py
+++ move_robot.py
@@ -1,8 +1,10 @@
+import copy
 import sys
 import rospy
 import moveit_commander
 import moveit_msgs.msg
 import tf
+import time
 
 #Init
 
@@ -31,7 +33,11 @@ display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path
 def move_robot(x,y,z, debug=False):
     if debug:
         input("Start")
-    group.set_planner_id("RRTConnect")
+    group.set_planner_id("EMR")
+    group.allow_replanning(True)
+    group.set_goal_tolerance(0.005)
+    group.set_num_planning_attempts(3)
+    group.set_planning_time(1)
     #We can set RRTConnect here, however:
     #we have do define the parameters like in RVIZ because otherwise the robot is doing crazy things as always
     #longest_valid_segment: 0.005
@@ -40,12 +46,14 @@ def move_robot(x,y,z, debug=False):
     pose_goal = group.get_current_pose()
     pose_goal.pose.position.x  +=x # from tf-Tree
     pose_goal.pose.position.y  +=y # from tf-Tree
-    #pose_goal.pose.position.z  +=z# from tf-Tree
+    pose_goal.pose.position.z  +=z# from tf-Tree
 
     print(" going to ", pose_goal.pose.position)
     #input("confirm moving ur3_arm to this position")
+    t1 = time.time()
     group.set_pose_target(pose_goal)
-    group.go(wait=False)
+    group.go(wait=True)
+    print(t1 - time.time())
     #sucess = group.go(wait=True)
     #print("suc?", sucess)
     group.stop()
@@ -53,5 +61,42 @@ def move_robot(x,y,z, debug=False):
 
     return
 
+def move_cartesian(x,y,z, debug=False):
+
+    if debug:
+        input("Start")
+    group.set_planner_id("EMR")
+    group.allow_replanning(True)
+    group.set_goal_tolerance(0.005)
+    group.set_num_planning_attempts(1)
+    group.set_planning_time(1)
+
+    waypoints = []
+
+    wpose = group.get_current_pose().pose
+    wpose.position.x +=x
+    wpose.position.y +=y 
+    wpose.position.z +=z  
+    waypoints.append(copy.deepcopy(wpose))
+
+    # We want the Cartesian path to be interpolated at a resolution of 1 cm
+    # which is why we will specify 0.01 as the eef_step in Cartesian
+    # translation.  We will disable the jump threshold by setting it to 0.0 disabling:
+    (plan, fraction) = group.compute_cartesian_path(
+                                    waypoints,   # waypoints to follow
+                                    0.01,        # eef_step
+                                    0.0)         # jump_threshold
+
+    # Note: We are just planning, not asking move_group to actually move the robot yet:
+    t1 = time.time()
+    group.execute(plan, wait=True)
+    print(t1 - time.time())
+    #sucess = group.go(wait=True)
+    #print("suc?", sucess)
+    group.stop()
+    group.clear_pose_targets()
+    return
+
 if __name__ == "__main__":
-    move_robot(0.1, 0.0, 0.0, True)
+    #move_robot(0.1, 0.1, 0.1, True)
+    move_cartesian(+0.2, +0.3, -0.2, True)
