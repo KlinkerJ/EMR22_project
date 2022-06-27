a4b8cb785f6c28a2a270e02cc93a866a03c52f23
lennart2810 <fuhrig.lennart@web.de>
2022-06-27 21:32:45 +0200

    Setup as a ROS Package

---
+++ nodes/move_robot.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+import copy
+import sys
+import rospy
+import moveit_commander
+import moveit_msgs.msg
+import tf
+import time
+
+#Init
+
+# First initialize moveit_ Command and rospy nodes:
+moveit_commander.roscpp_initialize(sys.argv)
+rospy.init_node('move_group_python_interface',
+                anonymous=True)
+# Instantiate the robot commander object,
+# which is used to control the whole robot
+robot = moveit_commander.RobotCommander()
+
+# Instantiate the MoveGroupCommander object.
+group_name = "ur5_arm"
+group = moveit_commander.MoveGroupCommander(group_name)
+group_name_gripper = "gripper"
+group_gripper = moveit_commander.MoveGroupCommander(group_name_gripper)
+
+# Create a Publisher.
+display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
+                                               moveit_msgs.msg.DisplayTrajectory,
+                                               queue_size=20)
+
+#listener = tf.TransformListener()
+#(trans,rot) = listener.lookupTransform('/world', '/object_9', rospy.Time())
+
+def move_robot(x,y,z, debug=False, wait=True):
+    if debug:
+        input("Start")
+    group.set_planner_id("EMR")
+    group.allow_replanning(True)
+    group.set_goal_tolerance(0.005)
+    group.set_num_planning_attempts(3)
+    group.set_planning_time(1)
+    #We can set RRTConnect here, however:
+    #we have do define the parameters like in RVIZ because otherwise the robot is doing crazy things as always
+    #longest_valid_segment: 0.005
+    #projection_evaluator: joints(shoulder_pan_joint,shoulder_lift_joint)
+    #range: 0
+    pose_goal = group.get_current_pose()
+    pose_goal.pose.position.x  +=x # from tf-Tree
+    pose_goal.pose.position.y  +=y # from tf-Tree
+    pose_goal.pose.position.z  +=z# from tf-Tree
+
+    print(" going to ", pose_goal.pose.position)
+    #input("confirm moving ur3_arm to this position")
+    t1 = time.time()
+    group.set_pose_target(pose_goal)
+    group.go(wait=wait)
+    print(t1 - time.time())
+    #sucess = group.go(wait=True)
+    #print("suc?", sucess)
+    group.stop()
+    group.clear_pose_targets()
+
+    return
+
+def move_cartesian(x,y,z, debug=False, wait=True, stopBefore=False):
+
+    if debug:
+        input("Start")
+    if stopBefore: group.stop()
+    group.set_planner_id("EMR")
+    group.allow_replanning(True)
+    group.set_goal_tolerance(0.005)
+    group.set_num_planning_attempts(1)
+    group.set_planning_time(1)
+
+    waypoints = []
+
+    wpose = group.get_current_pose().pose
+    wpose.position.x +=x
+    wpose.position.y +=y 
+    wpose.position.z +=z  
+    waypoints.append(copy.deepcopy(wpose))
+
+    # We want the Cartesian path to be interpolated at a resolution of 1 cm
+    # which is why we will specify 0.01 as the eef_step in Cartesian
+    # translation.  We will disable the jump threshold by setting it to 0.0 disabling:
+    (plan, fraction) = group.compute_cartesian_path(
+                                    waypoints,   # waypoints to follow
+                                    0.01,        # eef_step
+                                    0.0)         # jump_threshold
+
+    # Note: We are just planning, not asking move_group to actually move the robot yet:
+    t1 = time.time()
+    group.execute(plan, wait=wait)
+    print(t1 - time.time())
+    #sucess = group.go(wait=True)
+    #print("suc?", sucess)
+    #group.stop()
+    group.clear_pose_targets()
+    return
+
+if __name__ == "__main__":
+    #move_robot(-0.1, 0.1, 0.1, True)
+    move_cartesian(0.2, +0.3, -0.2, True)
---
+++ nodes/yolo_img_pub.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+
+import rospy
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import cv2
+import torch
+    
+
+def talker(object_str = "bottle", debug = False):
+    # init node
+    rospy.init_node('yolo_image_node', anonymous=False)
+    pub = rospy.Publisher('/yolo_prediction_img', Image, queue_size=1)
+
+    # init yolo model
+    model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+
+    # init camera and cv2
+    bridge = CvBridge()
+    cap = cv2.VideoCapture(0)
+    while not cap.isOpened():
+        rospy.sleep(1)
+    print("VideoCapture.isOpened: ", cap.isOpened())
+
+    # get frame info (shape and centerpoint)
+    ret, frame = cap.read()
+    frame_height = frame.shape[0]
+    frame_width  = frame.shape[1]
+    print("Frame Dimensions: ", frame_height, frame_width)
+    center_point = [frame_width / 2, frame_height / 2]
+    print("Center-Point: ", center_point)
+
+    # run node
+    while not rospy.is_shutdown():
+        # read image
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        # get model prediction
+        results = model(frame)
+        res = results.pandas().xyxy[0]
+        if object_str in list(res.name):
+                #detected object --> calculate position
+                index = list(res.name).index(object_str)
+                x_min, y_min = list(res.xmin)[index], list(res.ymin)[index]
+                x_max, y_max = list(res.xmax)[index], list(res.ymax)[index]
+                x_mid = x_min + 0.5 * (x_max - x_min)
+                y_mid = y_min + 0.5 * (y_max - y_min)
+
+                #delta from Center
+                delta_x, delta_y = x_mid - center_point[0], center_point[1] - y_mid
+                #positive x -> ball right from Center
+                #positive y -> ball higher than Center
+                
+                # draw bottle center point and object name on frame
+                frame = cv2.circle(frame,(int(x_mid), int(y_mid)), 10, (0,0,255), -1)
+                frame = cv2.putText(frame, object_str, (int(x_mid)+20, int(y_mid)+10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2, cv2.LINE_AA)
+
+                if debug:
+                    print("Object Position:", x_mid, y_mid)
+                    print("Delta from Center:", delta_x, delta_y)
+
+        # pub img
+        msg = bridge.cv2_to_imgmsg(frame, 'bgr8')
+        pub.publish(msg)
+
+        if rospy.is_shutdown():
+            cap.release()
+
+if __name__ == '__main__':
+    try:
+        talker(object_str = "bottle", debug = False)
+    except rospy.ROSInterruptException:
+        pass
